7877623ee60a732cf0bcf434a549cab0bcd34c20
Cedric Deschenes <cedricdeschenes@gmail.com>
2015-05-11 11:12:07 -0400

    Allowed to add a new image in a word template

--- src/PhpWord/Template.php
+++ src/PhpWord/Template.php
@@ -446,4 +446,26 @@ class Template
         }
         return substr($this->documentXML, $startPosition, ($endPosition - $startPosition));
     }
+
+
+    /**
+     * Set a new image
+     *
+     * @param string $search
+     * @param string $replace
+     */
+    public function setImageValue($search, $replace)
+    {
+        // Sanity check
+        if (!file_exists($replace))
+        {
+            return;
+        }
+
+        // Delete current image
+        $this->zipClass->deleteName('word/media/' . $search);
+
+        // Add a new one
+        $this->zipClass->addFile($replace, 'word/media/' . $search);
+    }
 }
